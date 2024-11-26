b3a12df0fcd761311e54368e17720612618f1721
Devanshu Gupta <devanshu9167@gmail.com>
2024-11-26 21:51:04 +0530

    Media query and customization

---
+++ Tailwind2/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+   colors:{
+    transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+   },
+   borderColor:{
+    "black":"#000000"
+   },
+   fontWeight:{
+    "xl":"900"
+   },
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
+

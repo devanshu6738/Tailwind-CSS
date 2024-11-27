2df6a0a576ee2e212105b6d91714089bd27175c0
Devanshu Gupta <devanshu9167@gmail.com>
2024-11-27 16:26:53 +0530

    creating a navigation bar through tailwindCSS

---
+++ Paytm Clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "bullu":"#00BAF2"
+      }
+    },
+  },
+  plugins: [],
+}
+
